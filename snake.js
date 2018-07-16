77b311a9d9e93b01fd261910802440edc7789d13
Jeremy Kimble <31196158+thefewunshaken@users.noreply.github.com>
2018-07-15 19:57:09 -0500

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,33 +1,88 @@
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const blockWidth = 10;
+const blockHeight = 10;
+const generateBlock = (x,y) => ctx.fillRect(x,y,blockWidth,blockHeight);
 
+//return the distance between any two points on the canvas using the pythagorean theorem.
+const dist = (x,y,x2,y2) => {
+	xDistance = x - x2;
+	yDistance = y - y2;
 
-const food = (x,y,w,h) => {
-	this.position = [x, y];
-	this.size = [w, h];
-
-
-
-	this.generate = () => {
-		// random position property every time after snake.eat() is called
-
-	}
-
+	return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
-const snake = (x,y,dx,dy,w,h) => {
-	this.position[x,y];
-	this.size = [w,h];
-	this.dx = dx;
-	this.dy = dy;
 
+const xyRand = () => {
+	let numRange = Math.floor(Math.random() * 40);
+		return numRange * blockWidth;
+}
 
-	this.eat = () => {
-		// triggered when snake.position = food.position
+/**------------------------------FOOD OBJECT CONSTRUCTOR---------------------**/
+class Food {
+	constructor(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+	// this.width = width;
+	// this.height = height;
+
+	//generate the food for the snake.
+	generate() {
+		// won't we need to erase old food somehow before creating a new one?
+		ctx.beginPath();
+		ctx.fillStyle = "yellow";
+		generateBlock(this.x, this.y);
+		ctx.closePath();
+
+	}
+	//check if the snake has eaten the food. If it has, generate a new food object.
+	hasSnakeCollided(snakeX,snakeY) {
+		if(dist(snakeX,snakeY,this.x,this.y) !== 0){
+			return false;
+
+		}
+			this.generate();
+		
+		}
 
 	}
 
-	this.initialize = () => {
-		// determines initial size and position for snake
+/**-----------------------------------SNAKE OBJECT CONSTRUCTOR-----------------------------**/
+const snake = (x,y,dx,dy,width,height) => {
+	this.x = x;
+	this.y = y;
+	this.speed = [dx,dy];
+
+	this.draw = () => {
+			ctx.beginPath();
+			ctx.fillStyle = '#00dc00';
+			generateBlock(this.x, this.y);
+			ctx.closePath();
+	}
+	
+	this.update = () => {
+		//Logic for snake's behavior and Boundaries go here.
+		this.draw();
 	}
 }
+
+const init = () => {
+	//initialize the objects from the constructors above.
+	const newFood = new Food(xyRand(), xyRand())
+	canvas.height = 400;
+	canvas.width = 400;
+	newFood.generate();
+	console.log(newFood);
+	requestAnimationFrame(animate);
+	console.log('game has been initialized.');
+}
+const animate = () => {
+	 //add some behavior if you want :)
+	requestAnimationFrame(animate);
+}
+
+init();
+
+// have animate in an eventlistener to prevent game from launching immediately
+animate();
