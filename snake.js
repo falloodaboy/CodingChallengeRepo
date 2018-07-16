2610d7c7cd8518ef077435db6733f5d74169d5e1
Jeremy Kimble <kimblejeremy18@gmail.com>
2018-07-15 23:48:23 -0500

    refactored some of snake.js

--- snake.js
+++ snake.js
@@ -6,6 +6,7 @@ const blockHeight = 10;
 const generateBlock = (x,y) => ctx.fillRect(x,y,blockWidth,blockHeight);
 const generateStroke = (x,y) => ctx.strokeRect(x,y,blockWidth,blockHeight);
 let snakeLength = 4;
+let leftPressed, rightPressed, upPressed, downPressed = false;
 
 //return the distance between any two points on the canvas using the pythagorean theorem.
 const dist = (x,y,x2,y2) => {
@@ -75,55 +76,84 @@ function snake(x,y) {
 	}
 }
 
+
 const init = () => {
 	//initialize the objects from the constructor functions above.
 	canvas.height = 400;
 	canvas.width = 400;
-
+	
 	const newFood = new food(xyRand(), xyRand());
 	const newSnake = new snake(150, 150);
 	newSnake.draw();
 	newFood.generate();
 
-	// don't initialize if food and snake are in the same spot
+	// re-initialize if food and snake are in the same spot
 	const snakeBodyFilter = newSnake.snakeBody.filter(block => {
 		return (block.x === newFood.x && block.y === newFood.y);
 	});
 	if ( snakeBodyFilter.length > 0	) {
-		console.log(newFood.x, newSnake.x);
-		console.log('snake and food are in the same space');
+		console.log(`${newFood.x, newSnake.x} snake and food are in the same space`);
+		newFood.generate();
 	}
 	
-	console.log(newFood.x, snakeBodyFilter);
-	console.log(snakeBodyFilter.length);
-	console.log(newSnake.snakeBody);
+	// console.log(newSnake.snakeBody);
 	console.log('game has been initialized.');
 	ctx.save();
-	requestAnimationFrame(animate);
 }
 
 const animate = () => {
-
+	console.log('running');
 	//add some behavior if you want :)
 	requestAnimationFrame(animate);
 }
 
-const startGame = (event) => {
+init();
+
+const startGame = event => {
 	if(event.which === 13) {
 		console.log('game has begun');
-		animate();
+		requestAnimationFrame(animate);
 	}
 }
 
-const restartGame = (event) => {
+const restartGame = event => {
 	if(event.which === 32 || event.key === 'r' || event.which === 82) {
 		ctx.restore();
 		init();
 	}
 }
 
-init();
+const keyDownHandler = event => {
+	if(event.which === 37 || event.which === 65) {
+		leftPressed = true;
+	}
+	else if(event.which === 39 || event.which === 68) {
+		rightPressed = true;
+	}
+	else if(event.which === 38 || event.which === 87) {
+		upPressed = true;
+	}
+	else if(event.which === 40 || event.which === 83) {
+		downPressed = true;
+	}
+}
 
-// have animate in an eventlistener to prevent game from launching immediately
+const keyUpHandler = event => {
+	if(event.which === 37 || event.which === 65) {
+		leftPressed = false;
+	}
+	else if(event.which === 39 || event.which === 68) {
+		rightPressed = false;
+	}
+	else if(event.which === 38 || event.which === 87) {
+		upPressed = false;
+	}
+	else if(event.which === 40 || event.which === 83) {
+		downPressed = false;
+	}
+}
+
 window.addEventListener("keypress", startGame);
 window.addEventListener("keypress", restartGame);
+window.addEventListener("keydown", keyDownHandler);
+window.addEventListener("keyup", keyUpHandler);
