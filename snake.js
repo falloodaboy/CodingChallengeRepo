8364b5c4d760ef633ac3f9aa60633031b3967f25
falloodaboy <40337476+falloodaboy@users.noreply.github.com>
2018-07-15 20:07:54 -0400

    Some fixes in snake.js

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 
 //return the distance between any two points on the canvas using the pythagorean theorem.
 const dist = (x,y,x2,y2) => {
-	xDistance = x - x2;
+	xDistance = x - x2; 
 	yDistance = y - y2;
 
 	return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
@@ -12,15 +12,17 @@ const dist = (x,y,x2,y2) => {
 
 /**------------------------------FOOD OBJECT CONSTRUCTOR---------------------**/
 const food = (x,y,width,height) => {
-	this.position = [x,y];
-	this.size = [width,height];
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
 
 
 	//generate the food for the snake.
 	this.generate = () => {
 		ctx.beginPath();
 		ctx.fillStyle = "yellow";
-		ctx.fillRect(this.position[x], this.position[y], this.size[width],this.size[height]);
+		ctx.fillRect(this.x,this.y,this.width,this.height);
 		ctx.closePath();
 
 	}
@@ -30,7 +32,7 @@ const food = (x,y,width,height) => {
 		this.snakeX = snakeX;
 		this.snakeY = snakeY;
 
-		if(dist(this.snakeX,this.snakeY,this.position[x],this.position[y]) !== 0){
+		if(dist(this.snakeX,this.snakeY,this.x,this.y) !== 0){
 			return false;
 
 		}
@@ -47,14 +49,17 @@ const food = (x,y,width,height) => {
 
 /**-----------------------------------SNAKE OBJECT CONSTRUCTOR-----------------------------**/
 const snake = (x,y,dx,dy,width,height) => {
-	this.position = [x,y];
-	this.speed = [dx,dy];
-	this.size = [width, height];
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.dx = dx;
+	this.dy = dy;
 
 	this.draw = () => {
 			ctx.beginPath();
 			ctx.fillStyle = 'green';
-			ctx.fillRect(this.position[x], this.position,this.size[width],this.size[height]);
+			ctx.fillRect(this.x,this.y,this.width,this.height);
 			ctx.closePath();
 	}
 	
