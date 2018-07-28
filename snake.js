b332e29335cc176f4929426c68aa56f89e15277a
Jeremy Kimble <kimblejeremy18@gmail.com>
2018-07-27 21:55:35 -0500

    completed basic functionality, added score and high score counter

--- snake.js
+++ snake.js
@@ -1,159 +1,244 @@
-
+const scoreCount = document.querySelector('.scoreCount');
+const highScore = document.querySelector('.highScore');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+canvas.height = 400;
+canvas.width = 400;
 const blockWidth = 10;
 const blockHeight = 10;
 const generateBlock = (x,y) => ctx.fillRect(x,y,blockWidth,blockHeight);
 const generateStroke = (x,y) => ctx.strokeRect(x,y,blockWidth,blockHeight);
-let snakeLength = 4;
-let leftPressed, rightPressed, upPressed, downPressed = false;
+let direction;
+let score;
+let looper;
+let speed = 100;
+let gameRunning = false;
 
-//return the distance between any two points on the canvas using the pythagorean theorem.
-const dist = (x,y,x2,y2) => {
-	xDistance = x - x2;
-	yDistance = y - y2;
-
-	return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-}
 
 const xyRand = () => {
 	let numRange = Math.floor(Math.random() * 40);
 		return numRange * blockWidth;
 }
 
+const scoreCheck = () => {
+	let currentScore = scoreCount.innerHTML.match(/\d+/); //ex 0 or [0, 1]
+	let currentHighScore = highScore.innerHTML.match(/\d+/);
+	console.log(currentScore, currentHighScore);
+	if(currentScore instanceof Array || currentHighScore instanceof Array) {
+		currentScore = parseInt(currentScore.join(""));
+		currentHighScore = parseInt(currentHighScore.join(""));
+	}
+	if(currentScore > currentHighScore) {
+		highScore.innerHTML = `High Score: ${score}`;
+		console.log('New high score');
+	}
+	console.log(currentScore, currentHighScore);
+}
+
 /**------------------------------FOOD OBJECT CONSTRUCTOR---------------------**/
-function food(x,y) {
+function food() {
+		this.x;
+		this.y;
+
+	this.draw = (x, y) => {
 		this.x = x;
 		this.y = y;
-
-	//generate the food for the snake.
-	this.generate = () => {
-		// won't we need to erase old food somehow before creating a new one?
-		ctx.clearRect(x,y,10,10);
-		ctx.beginPath();
 		ctx.fillStyle = "yellow";
-		generateBlock(this.x, this.y);
-		ctx.closePath();
+		generateBlock(x, y);
 	}
-	//check if the snake has eaten the food. If it has, generate a new food object.
-	this.hasSnakeCollided = (snakeX,snakeY) => {
-		if(dist(snakeX,snakeY,this.x,this.y) !== 0){
-			return false;
-
-		}
-			this.generate();
-			snakeLength++;
-		}
-
-	}
-
-/**-----------------------------------SNAKE OBJECT CONSTRUCTOR-----------------------------**/
-function snake(x,y) {
-	this.x = x;
-	this.y = y;
-	this.blockCount = 4;
-	this.offset = 10;
-	// this.speed = [dx,dy];
-	this.snakeBody = [];
-
-	this.draw = () => {
-		ctx.beginPath();
-		ctx.fillStyle = '#00dc00';
-		ctx.strokeStyle = 'black';
-			for (let i = 0; i < this.blockCount; i++) {
-				this.snakeBody[i] = { x: x, y: y };
-				generateBlock(x, y);
-				generateStroke(x, y, 10, 10);
-				x-=this.offset;
-			}
-		// console.log(this.snakeBody);
-		ctx.closePath();
-	}
-	
-	this.update = () => {
-		//Logic for snake's behavior and Boundaries go here.
-		this.draw();
+	//check if the snake has eaten the food
+	this.hasSnakeCollided = () => {
+		const snakeBodyFilter = firstSnake.snakeBody.filter(block => {
+			return (block.x === this.x && block.y === this.y);
+		});
+		if ( snakeBodyFilter.length > 0	) {
+			console.log('Food was eaten.');
+			return true;
+		} 
+		return false;
 	}
 }
+
+/**-----------------------------------SNAKE OBJECT CONSTRUCTOR-----------------------------**/
+function snake() {
+	this.snakeBody = [];
+
+	this.draw = (x, y) => {
+			ctx.strokeStyle = "black";
+			generateStroke(x, y);
+			ctx.fillStyle = '#00dc00';
+			generateBlock(x, y);
+		}
+
+	this.initSnake = () => {
+		this.snakeBody = [];
+		let length = 3;
+		let num = 0;
+		for(let i = 0; i < length; i++) {
+			this.snakeBody.push({x: 300, y: 100 - num});
+			num+=10;
+		}
+		// this.snakeBody.push({x: 300, y: 80}, {x: 300, y: 90}, {x: 300, y: 100});
+	}
+		
+		this.update = () => {
+			this.checkCollision(this.snakeBody);
+			let tail;
+		
+			if(newFood.hasSnakeCollided() ) {
+				tail = {x: newFood.x, y: newFood.y};
+				this.snakeBody.unshift(tail);
+				score++;
+				console.log(`Score: ${score}`);
+				newFood.draw(xyRand(), xyRand());
+			} else {
+					tail = this.snakeBody.pop();
+					tail.x = this.snakeBody[0].x;
+					tail.y = this.snakeBody[0].y;
+					this.snakeBody.unshift(tail);
+				}
+		}
+
+		this.checkCollision = (array) => {
+			array.forEach(block => {
+					if (
+						// out of bounds
+						block.y < 0 || block.y > 390 || block.x < 0 || block.x > 390 ||
+						// or if snake collides itself
+						// NOT WORKING ON THE TAIL
+						((block.x === this.snakeBody[0].x && block.y === this.snakeBody[0].y) && block !== this.snakeBody[0])
+					) {
+						if((firstSnake.snakeBody[0].x === firstSnake.snakeBody[firstSnake.snakeBody.length-1].x) && (firstSnake.snakeBody[0].y === firstSnake.snakeBody[firstSnake.snakeBody.length-1].y)) {
+							console.log("Got your tail");
+						}
+						stopGame();
+					}
+			});
+		}
+
+}
+
+
+let firstSnake = new snake();
+let newFood = new food();
 
 
 const init = () => {
-	//initialize the objects from the constructor functions above.
-	canvas.height = 400;
-	canvas.width = 400;
-	
-	const newFood = new food(xyRand(), xyRand());
-	const newSnake = new snake(150, 150);
-	newSnake.draw();
-	newFood.generate();
-
-	// re-initialize if food and snake are in the same spot
-	const snakeBodyFilter = newSnake.snakeBody.filter(block => {
-		return (block.x === newFood.x && block.y === newFood.y);
+	firstSnake.initSnake();
+	firstSnake.snakeBody.forEach(block => {
+		firstSnake.draw(block.x, block.y);
 	});
-	if ( snakeBodyFilter.length > 0	) {
-		console.log(`${newFood.x, newSnake.x} snake and food are in the same space`);
-		newFood.generate();
+	newFood.draw(xyRand(), xyRand());
+	direction = 'down';
+	score = 0;
+	
+	// re-initialize if food and snake are in the same spot
+	if(newFood.hasSnakeCollided()) {
+		// clearCanvas();
+		newFood.draw(xyRand(), xyRand());
 	}
 	
-	// console.log(newSnake.snakeBody);
+	// console.log(firstSnake.snakeBody);
 	console.log('game has been initialized.');
-	ctx.save();
 }
 
 const animate = () => {
-	console.log('running');
-	//add some behavior if you want :)
-	requestAnimationFrame(animate);
+	clearCanvas();
+	window.addEventListener("keydown", directionHandler);
+	switch(direction) {
+		case 'left':
+		firstSnake.snakeBody[0].x-=10;
+		break;
+		case 'right':
+		firstSnake.snakeBody[0].x+=10;
+		break;
+		case 'up':
+		firstSnake.snakeBody[0].y-=10;
+		break;
+		case 'down':
+		firstSnake.snakeBody[0].y+=10;
+		break;
+	}
+	
+	newFood.draw(newFood.x, newFood.y);
+
+	firstSnake.snakeBody.forEach(block => {
+		firstSnake.draw(block.x, block.y);
+	});
+
+	firstSnake.update();
+
+	scoreCount.innerHTML = `Score: ${score}`;
+	// highScore.innerHTML = `High Score: 0`;
+
+	// console.log(`snake length: ${firstSnake.snakeBody.length}`);
+	// console.log(firstSnake.snakeBody);
+	// console.log(firstSnake.snakeBody[2]);
+
 }
 
-init();
-
 const startGame = event => {
-	if(event.which === 13) {
-		console.log('game has begun');
-		requestAnimationFrame(animate);
+	if(event.which === 13 && !gameRunning) {
+		init();
+		// momentary pause before game starts
+		setTimeout(() => {
+			looper = setInterval(animate, speed);
+			gameRunning = true;
+			console.log("game running");
+		},200);
 	}
+}
+
+const stopGame = () => {
+	clearInterval(looper);
+	clearCanvas();
+	gameRunning = false;
+	window.removeEventListener("keydown", directionHandler);
+	console.log("game not running");
+	scoreCheck();
 }
 
 const restartGame = event => {
 	if(event.which === 32 || event.key === 'r' || event.which === 82) {
-		ctx.restore();
-		init();
+		stopGame();
 	}
 }
 
-const keyDownHandler = event => {
-	if(event.which === 37 || event.which === 65) {
-		leftPressed = true;
-	}
-	else if(event.which === 39 || event.which === 68) {
-		rightPressed = true;
-	}
-	else if(event.which === 38 || event.which === 87) {
-		upPressed = true;
-	}
-	else if(event.which === 40 || event.which === 83) {
-		downPressed = true;
-	}
+const clearCanvas = () => {
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.fillStyle = "black";
+	ctx.fillRect(0,0,300,400);
 }
 
-const keyUpHandler = event => {
-	if(event.which === 37 || event.which === 65) {
-		leftPressed = false;
+const directionHandler = event => {
+	switch(event.which){
+		case 37:
+		case 65:
+		if(direction !== 'right') {
+			direction = 'left';
+		}
+		break;
+		case 39:
+		case 68:
+		if(direction !== 'left') {
+			direction = 'right';
+		}
+		break;
+		case 38:
+		case 87:
+		if(direction !== 'down') {
+			direction = 'up';
+		}
+		break;
+		case 40:
+		case 83:
+		if(direction !== 'up') {
+			direction = 'down';
+		}
+		break;
 	}
-	else if(event.which === 39 || event.which === 68) {
-		rightPressed = false;
-	}
-	else if(event.which === 38 || event.which === 87) {
-		upPressed = false;
-	}
-	else if(event.which === 40 || event.which === 83) {
-		downPressed = false;
-	}
+	console.log(direction);
 }
 
 window.addEventListener("keypress", startGame);
 window.addEventListener("keypress", restartGame);
-window.addEventListener("keydown", keyDownHandler);
-window.addEventListener("keyup", keyUpHandler);
